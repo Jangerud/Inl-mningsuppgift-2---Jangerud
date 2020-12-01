c06653ddf941d15e376f8abc5e42d2d632d2b957
jange <jange@DESKTOP-1PGUA82>
2020-12-01 11:13:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Inlämningsuppgift_2___Jangerud
+{
+
+  
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+
+
+        static string WelcomeMessage()
+        {
+            Console.BackgroundColor
+            Console.WriteLine("Hello User 5471!");
+            Console.WriteLine("Please enter passcode: ");
+            int userNumber = 
+
+        }
+    }
+}
